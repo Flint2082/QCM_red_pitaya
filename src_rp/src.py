6367e378b40b8b61d43c6c6779bd8578c04040c2
Flint Wardenaar <wardenaar@physics.leidenuniv.nl>
2026-02-16 17:15:59 +0100

    Adjusted OPC client code to be compatible with the existing system

--- src_rp/src.py
+++ src_rp/src.py
@@ -1,6 +1,7 @@
 from opcua import Client
 import time
 
+import wago_client
 import QCM_interface 
 
 # Server endpoint (must match server)
@@ -14,19 +15,35 @@ print("Connected to OPC UA server")
 qcm = QCM_interface.QCMInterface()
 qcm.startup()
 
+wago = wago_client.WagoClient(url = url)
+
+node_id = "ns=4;s=|var|750-8000 Basic Controller 100 2ETH ECO.Application.GVL_OPCUA.in."
+
+
 try:
     # Resolve namespace index dynamically
-    uri = "urn:qcm:redpitaya"
+    uri = "urn:wago-server"
     idx = client.get_namespace_index(uri)
 
     # Get the Frequency node
-    freq_node = client.get_node(f"ns={idx};s=QCM.Frequency")
+    freq_M_node = wago.get_node(f"ns={idx};s=QCM.Frequency.M")
+    freq_T_node = wago.get_node(f"ns={idx};s=QCM.Frequency.T")
+    
 
     # Read values in a loop
-    while True:
-        cur_freq = qcm.getFreq(1)
-        print(f"Current Frequency: {cur_freq}")
-        freq_node.set_value(cur_freq)
+    while True: 
+        freq_M = qcm.getFreq(1)
+        freq_T = qcm.getFreq(2)
+        
+        
+        wago.set_value(freq_M_node, freq_M)
+        wago.set_value(freq_T_node, freq_T)
+        
+        
+        print(f"Frequency M: {freq_M}, Frequency T: {freq_T}")
+                
+        
+        
         time.sleep(1)
 
 finally:
--- src_server/test_server.py
+++ src_server/test_server.py
@@ -6,7 +6,7 @@ server.set_endpoint("opc.tcp://0.0.0.0:4840")
 
 server.set_server_name("QCM OPCUA Server")
 
-uri = "urn:qcm:redpitaya"
+uri = "urn:wago-server"
 idx = server.register_namespace(uri)
 
 objects = server.get_objects_node()
@@ -15,20 +15,43 @@ qcm = objects.add_object(
     "QCM"
 )
 
-freq_node = qcm.add_variable(
-    ua.NodeId("QCM.Frequency", idx),
-    "Frequency", 
+freq_M_node = qcm.add_variable(
+    ua.NodeId("QCM.Frequency.M", idx),
+    "Frequency M", 
     0.0
 )
-freq_node.set_writable()
+freq_M_node.set_writable()
 
+freq_T_node = qcm.add_variable(
+    ua.NodeId("QCM.Frequency.T", idx),
+    "Frequency T", 
+    0.0
+)
+freq_T_node.set_writable()
+
+temp_node = qcm.add_variable(
+    ua.NodeId("QCM.Temperature", idx),
+    "Temperature", 
+    0.0
+)
+temp_node.set_writable()
+
+thickness_node = qcm.add_variable(
+    ua.NodeId("QCM.Thickness", idx),
+    "Thickness", 
+    0.0
+)
+thickness_node.set_writable()
+
+    
 server.start()
 print("OPC UA server started")
 
 try:
     while True:
-        freq = freq_node.get_value()
-        print(f"Frequency: {freq}")
+        freq_M = freq_M_node.get_value()
+        freq_T = freq_T_node.get_value()
+        print(f"Frequency M: {freq_M}, Frequency T: {freq_T}")
         time.sleep(1)
 
 finally:

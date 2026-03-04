a5d6f0c35c72554f50a8ec6147ec209039406ca1
Flint Wardenaar <wardenaar@physics.leidenuniv.nl>
2026-03-04 15:39:28 +0100

    added all the nodes to the main,py

--- src/main.py
+++ src/main.py
@@ -21,6 +21,7 @@ url = "opc.tcp://132.229.46.113:4840"
 
 #rp_ip = QCM_interface.find_red_pitaya(subnet= "132.229.46.")
 rp_ip = "132.229.46.164"
+#rp_ip = "192.168.1.55"
 
 qcm = QCM_interface.QCMInterface(rp_ip)
 qcm.startup()
@@ -33,31 +34,43 @@ node_id_base = "|var|750-8000 Basic Controller 100 2ETH ECO.Application.GVL_OPCU
 
 try:
     # Resolve namespace index dynamically
-    uri = "urn:wago-client"
+    uri = wago.client.application_uri
     idx = wago.client.get_namespace_index(uri)
 
 
     #setRef = wago.get_node(f"ns={idx};s=QCM.SetRef")
     
-    # get frequency window nodes
-    #window_M_node = wago.get_node(f"ns={idx};s=QCM.Window.M")
-    #window_T_node = wago.get_node(f"ns={idx};s=QCM.Window.T")
+    # get object nodes
+    qcm_node = wago.get_node(ua.NodeId(node_id_base +               "QCM", idx))
+    
+    # All the CTRL nodes
+    ctrl_node = wago.get_node(ua.NodeId(node_id_base +              "QCM.CTRL", idx))
+    start_meas_node = wago.get_node(ua.NodeId(node_id_base +        "QCM.CTRL.StartMeasurement", idx))
+    stop_meas_node = wago.get_node(ua.NodeId(node_id_base +         "QCM.CTRL.StopMeasurement", idx))
+    set_zero_node = wago.get_node(ua.NodeId(node_id_base +          "QCM.CTRL.SetZero", idx))
+    sweep_node = wago.get_node(ua.NodeId(node_id_base +             "QCM.CTRL.Sweep", idx))
+    
+    # All the SET nodes
+    set_node = wago.get_node(ua.NodeId(node_id_base +               "QCM.SET", idx))
+    density_node = wago.get_node(ua.NodeId(node_id_base +           "QCM.SET.Density", idx))
+    z_ratio_node = wago.get_node(ua.NodeId(node_id_base +           "QCM.SET.Z-ratio", idx))
+    start_freq_node = wago.get_node(ua.NodeId(node_id_base +        "QCM.SET.StartFreq", idx))
+    ambient_temp_node = wago.get_node(ua.NodeId(node_id_base +      "QCM.SET.AmbientTemp", idx))
+    coeff_node = wago.get_node(ua.NodeId(node_id_base +             "QCM.SET.Coeff", idx))
+    
+    # All the READ nodes
+    get_node = wago.get_node(ua.NodeId(node_id_base +               "QCM.READ", idx))
+    freq_M_node = wago.get_node(ua.NodeId(node_id_base +            "QCM.READ.MassFrequency", idx))
+    freq_T_node = wago.get_node(ua.NodeId(node_id_base +            "QCM.READ.TempFrequency", idx))
+    temp_node = wago.get_node(ua.NodeId(node_id_base +              "QCM.READ.Temperature", idx))
+    comp_thickness_node = wago.get_node(ua.NodeId(node_id_base +    "QCM.READ.CompensatedThickness", idx))
+    uncomp_thickness_node = wago.get_node(ua.NodeId(node_id_base +  "QCM.READ.UncompensatedThickness", idx))
+    comp_rate_node = wago.get_node(ua.NodeId(node_id_base +         "QCM.READ.CompensatedRate", idx))
+    uncomp_rate_node = wago.get_node(ua.NodeId(node_id_base +       "QCM.READ.UncompensatedRate", idx))
+    Comp_M_freq_node = wago.get_node(ua.NodeId(node_id_base +       "QCM.READ.CompensatedMassFrequency", idx))
+    timestamp_node = wago.get_node(ua.NodeId(node_id_base +         "QCM.READ.Timestamp", idx))
+    error_node = wago.get_node(ua.NodeId(node_id_base +             "QCM.READ.Error", idx))
 
-    # Get the Frequency node
-    #freq_M_node = wago.get_node(f"ns={idx};s=QCM.Frequency.M")
-    #freq_T_node = wago.get_node(f"ns={idx};s=QCM.Frequency.T")
-    
-    #temp_node = wago.get_node(f"ns={idx};s=QCM.Temperature")
-    
-    thickness_node_id = ua.NodeId(node_id_base + "QCM.Thickness", idx)
-    
-    # if(wago.has_node(uri, node_id_base, key)):
-    #     print("The wago has the thickness node")
-    # else:
-    #     print("ERROR: The wago does not have the thickness node")
-    
-    
-    thickness_node = wago.get_node(thickness_node_id)
     
     
 
@@ -72,12 +85,12 @@ try:
         #freq_M = qcm.getFreq(1)
         #freq_T = qcm.getFreq(2)
         
-        thickness = qcm.getThicknessUncomp()
+        thickness = 6
         
         
         #wago.write_node(freq_M_node, freq_M)
         #wago.write_node(freq_T_node, freq_T)
-        wago.write_node(thickness_node, thickness)
+        wago.write_node(uncomp_thickness_node, thickness)
         
         
         print(f"Thickness measurement: {thickness} nm")
@@ -85,8 +98,7 @@ try:
                 
         
         
-        # time.sleep(1)
-
+        time.sleep(1)
 
 finally:
     wago.disconnect()

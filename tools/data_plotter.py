aa1786978efadbc9232c063a19a954ed4c6e0bc2
Flint Wardenaar <wardenaar@physics.leidenuniv.nl>
2026-03-13 15:48:48 +0100

    improved the plotting by averaging the reference sensor signal

--- tools/data_plotter.py
+++ tools/data_plotter.py
@@ -16,21 +16,35 @@ df = pd.read_csv(file_path, delimiter = " ")
 # Convert timestamp from milliseconds to seconds
 time_s = df["Timestamp(ms)"] / 1000.0
 
-calibration_factor = (df["GVL_OPCUA.out.deposited_thickness"].iloc[-100:] / df["GVL_QCM.READ.UncompensatedThickness"].iloc[-100:]).mean()
+window = 100  # Number of samples for moving average
+
+calibration_factor = (df["GVL_OPCUA.out.deposited_thickness"].iloc[-window:] / df["GVL_QCM.READ.CompensatedThickness"].iloc[-window:]).mean()
+
 
 
 # Extract temperature
 temperature = df["GVL_QCM.READ.Temperature"]
 reference_sensor_thickness = df["GVL_OPCUA.out.deposited_thickness"]
+reference_sensor_gaussian = reference_sensor_thickness.rolling(window=window, win_type='gaussian', min_periods=1, center=True).mean(std=window/6)
+reference_sensor_averaged = reference_sensor_thickness.rolling(window=window, min_periods=1).mean().shift(-window//2)  # Offset by half the window size (50 samples) earlier in timeings
 uncompensated_thickness = calibration_factor * df["GVL_QCM.READ.UncompensatedThickness"]
 compensated_thickness = calibration_factor * df["GVL_QCM.READ.CompensatedThickness"]
+
+reference_rate = reference_sensor_averaged.diff() / time_s.diff()
+uncompensated_rate = uncompensated_thickness.diff() / time_s.diff()
+compensated_rate = compensated_thickness.diff() / time_s.diff()
+
+
 error = uncompensated_thickness - compensated_thickness
 
 # Plot with separate y-axes for thickness and temperature
 fig, ax1 = plt.subplots()
-line_ref, = ax1.plot(time_s, reference_sensor_thickness, label="Reference Sensor Thickness")
+line_ref, = ax1.plot(time_s, reference_sensor_gaussian, label="Reference Sensor Thickness")
 line_uncomp, = ax1.plot(time_s, uncompensated_thickness, label="Uncompensated Thickness")
 line_comp, = ax1.plot(time_s, compensated_thickness, label="Compensated Thickness")
+# line_ref_rate, = ax1.plot(time_s, reference_rate, label="Reference Sensor Rate", linestyle='--')
+# line_uncomp_rate, = ax1.plot(time_s, uncompensated_rate, label="Uncompensated Rate", linestyle='--')
+# line_comp_rate, = ax1.plot(time_s, compensated_rate, label="Compensated Rate", linestyle='--')
 # ax1.plot(time_s, error, label="Error")
 
 ax2 = ax1.twinx()
